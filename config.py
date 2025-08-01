65d74603b6da41a5683ec287821202a19e4670cb
Matt <matt@Matts-MacBook-Pro.local>
2025-08-01 13:30:38 -0400

    Fix empty mail port and update app.yaml

--- config.py
+++ config.py
@@ -85,7 +85,7 @@ class Config:
     
     # Mail settings
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    MAIL_PORT = int(os.environ.get('MAIL_PORT', 587))
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 587)  # Handle empty string
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() == 'true'
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
